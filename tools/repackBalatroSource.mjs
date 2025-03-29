8e0895c9e096719e48b706e06893c579b85221f7
Jean-François Morin <2498456+jfmmm@users.noreply.github.com>
2025-03-29 10:28:55 -0400

    Fix repack

--- tools/repackBalatroSource.mjs
+++ tools/repackBalatroSource.mjs
@@ -1,13 +1,13 @@
 import sevenBin from '7zip-bin';
 import Seven from 'node-7z';
 import dotenvJson from 'dotenv-json';
+import chalk from 'chalk';
 
 dotenvJson();
 
 Seven.add(process.env.balatroExecutable, './src/*.lua', {
   recursive: true,
-  overwrite: 'true',
   $bin: sevenBin.path7za
 });
 
-console.log(chalk.green(`Unpacked ${process.env.balatroExecutable}.`));
+console.log(chalk.green(`Repacked ${process.env.balatroExecutable}.`));
